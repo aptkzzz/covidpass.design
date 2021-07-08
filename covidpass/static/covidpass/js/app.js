de3d587aeb0ea6622a1a62696965ab82787ac6a2
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2021-07-08 21:53:45 +0300

    Подгрузка сгенерированных QR-кодов и стили текста

--- covidpass/static/covidpass/js/app.js
+++ covidpass/static/covidpass/js/app.js
@@ -1,19 +1,13 @@
 'use strict';
-var QRCodes = [];
+var QRCodes = [
+    "data:image/gif;base64,R0lGODlhAQABAIAAAP7//wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+    "", "", "", "", ""
+];
 var defaultTexts = ["РОССИЯ\nБУДЕТ\nЗДОРОВОЙ", ""];
-var textStyles = ["normal", "bold", "italic", "italic bold"];
-var textFonts = ["sans-serif"];
-var textAlignments = [];
 var emojiSet = [];
 
 var currentEmojis = [];
-var currentQRCode = null;
-var currentText = defaultTexts[0];
-//var currentBackgroundColorIndex = 0;
-var currentFont = textFonts[0];
-var currentTextSize = 20;
-var currentTextStyle = textStyles[0];
-var currentTextAlignment = ["center", "center"];
+var currentQRCode = QRCodes[0];
 
 var canvasContainer = $('#canvasContainer');
 canvasContainer.height(canvasContainer.width() / 372 * 263);
@@ -50,11 +44,39 @@ var emojiLayer = new Konva.Layer({});
 
 var QRLayer = new Konva.Layer({});
 
+var QRRect = new Konva.Rect({
+    fill: '#ffffff',
+    cornerRadius: stage.width() / 64 * 3,
+    x: stage.width() / 4 * 3,
+    y: stage.height() / 8 * 5,
+    width: stage.width() / 32 * 9,
+    height: stage.width() / 32 * 9,
+    offsetX: stage.width() / 64 * 9,
+    offsetY: stage.width() / 64 * 9
+});
+
+QRLayer.add(QRRect);
+
+var qr_img = new Image();
+qr_img.src = currentQRCode;
+
+var QRImage = new Konva.Image({
+    image: qr_img,
+    x: stage.width() / 4 * 3,
+    y: stage.height() / 8 * 5,
+    width: stage.width() / 4,
+    height: stage.width() / 4,
+    offsetX: stage.width() / 8,
+    offsetY: stage.width() / 8
+});
+
+QRLayer.add(QRImage);
+
 var textNode = new Konva.Text({
-    fontFamily: currentFont,
-    fontSize: currentTextSize,
-    fontStyle: currentTextStyle,
-    text: currentText,
+    fontFamily: 'sans-serif',
+    fontSize: 60,
+    fontStyle: 'normal',
+    text: 'РОССИЯ\nБУДЕТ\nЗДОРОВОЙ',
     height: stage.height(),
     width: stage.width() / 2,
     x: 0,
@@ -67,14 +89,68 @@ var textNode = new Konva.Text({
 textLayer.add(textNode);
 stage.add(passportTemplateLayer);
 stage.add(textLayer);
+stage.add(QRLayer);
 
-function updateText(text) {
-    textNode.setAttr('text', text);
+function updateText(text, style, size) {
+    textNode.setAttrs({
+        text: text,
+        fontStyle: style,
+        fontSize: size
+    });
     textNode.draw();
 }
 
 function submitTextHandler(event) {
-    updateText($('#textInput').val());
+    updateText(
+        $('#textInput').val(),
+        $('#textStyle').val(),
+        Number($('#textSize').val())
+    );
 }
 
+function updateQRCodes() {
+    for (const index in [...Array(6).keys()]) {
+        $(`#QR${Number(index) + 1}`).attr('src', QRCodes[Number(index)]);
+    }
+}
+
+function addListenersToQRVariants() {
+    for (const index in [...Array(6).keys()]) {
+        $(`#QR${Number(index) + 1}`).click(QRCodeSelected);
+    }
+}
+
+function QRCodeSelected() {
+    qr_img.src = $(this).attr('src');
+    document.querySelector('#canvasContainer').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function QRGeneratorCallback(data) {
+    for (const index in [...Array(6).keys()]) {
+        QRCodes[Number(index)] = data[`image_${Number(index) + 1}`];
+    }
+    updateQRCodes();
+    $('.qr-variants-holder').css('max-height', '3000px');
+}
+
+function onCertificateFormSubmit(event) {
+    event.preventDefault();
+    var form = new FormData();
+    form.append('link', $('#certificateLink').val());
+
+    $.ajax({
+        type: 'POST',
+        dataType: "json",
+        processData: false,
+        contentType: false,
+        url: '/api/v1/qr/',
+        data: form,
+        success: QRGeneratorCallback
+    });
+}
+
+addListenersToQRVariants();
 $('#submitText').click(submitTextHandler);
+$('#certificateLinkForm').submit(onCertificateFormSubmit);
