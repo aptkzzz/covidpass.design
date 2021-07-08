5fc2c0451748fbf5b2bff7839281ca3b5c982b6e
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2021-07-08 20:09:35 +0300

    Упростил бекенд используя генераторы

--- covidpass/api/v1/views.py
+++ covidpass/api/v1/views.py
@@ -7,14 +7,24 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer, GappedSquareModuleDrawer, CircleModuleDrawer, \
     VerticalBarsDrawer, HorizontalBarsDrawer
 from qrcode.image.styles.colormasks import RadialGradiantColorMask, SolidFillColorMask
+from PIL import Image, ImageOps
 
 
 class GenerateQRView(CreateAPIView):
     """Генерация QR-кода"""
     serializer_class = QRSerializer
 
-    def generate_qr(self, cert_id: str, back_color=(255, 255, 255), front_color=(0, 0, 1)):
+    def generate_qr(self, cert_id: str):
         GOSUSLUGI_PATH = 'https://www.gosuslugi.ru/vaccine/cert/verify/'
+
+        GENERATOR_MAP = [
+            (1, None, {}),
+            (2, StyledPilImage, {"module_drawer": RoundedModuleDrawer()}),
+            (3, StyledPilImage, {"module_drawer": GappedSquareModuleDrawer()}),
+            (4, StyledPilImage, {"module_drawer": CircleModuleDrawer()}),
+            (5, StyledPilImage, {"module_drawer": VerticalBarsDrawer()}),
+            (6, StyledPilImage, {"module_drawer": HorizontalBarsDrawer()})
+        ]
 
         qr = qrcode.QRCode(
             version=1,
@@ -25,51 +35,30 @@ class GenerateQRView(CreateAPIView):
         qr.add_data(GOSUSLUGI_PATH + cert_id)
         # color_mask=SolidFillColorMask(back_color=back_color, front_color=front_color)
 
-        img_1 = qr.make_image()
-        img_1.save(f"static/qrs/{cert_id}_1.png")
-
-        img_2 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
-        img_2.save(f"static/qrs/{cert_id}_2.png")
-
-        img_2 = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer())
-        img_2.save(f"static/qrs/{cert_id}_3.png")
-
-        img_2 = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer())
-        img_2.save(f"static/qrs/{cert_id}_4.png")
-
-        img_2 = qr.make_image(image_factory=StyledPilImage, module_drawer=VerticalBarsDrawer())
-        img_2.save(f"static/qrs/{cert_id}_5.png")
-
-        img_2 = qr.make_image(image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer())
-        img_2.save(f"static/qrs/{cert_id}_6.png")
+        for index, factory, kwargs in GENERATOR_MAP:
+            img = qr.make_image(image_factory=factory, **kwargs)
+            img.save(f"static/qrs/{cert_id}_{index}.png")
 
     def create(self, request, *args, **kwargs):
         GOSUSLUGI_PATH = 'https://www.gosuslugi.ru/vaccine/cert/verify/'
 
         link = request.data['link']
-        if link[:45] != GOSUSLUGI_PATH:
+        if not link.startswith(GOSUSLUGI_PATH):
             return Response({
                 'error': 'Сертификат зарегистрирован не на официальном сайте Госуслуг.',
             })
-        cert_id = link[45:]
+        cert_id = link.split("/")[-1]
 
         # Генерация QR-кода
         self.generate_qr(cert_id)
 
-        qr_1_path = f'static/qrs/{cert_id}_1.png'
-        qr_2_path = f'static/qrs/{cert_id}_2.png'
-        qr_3_path = f'static/qrs/{cert_id}_3.png'
-        qr_4_path = f'static/qrs/{cert_id}_4.png'
-        qr_5_path = f'static/qrs/{cert_id}_5.png'
-        qr_6_path = f'static/qrs/{cert_id}_6.png'
-        QR.objects.create(link=link, qr_1=qr_1_path, qr_2=qr_2_path,  qr_3=qr_3_path, qr_4=qr_4_path,
-                          qr_5=qr_5_path, qr_6=qr_6_path)
+        kwargs = dict(zip(
+            [f"qr_{n}" for n in range(1, 7)],
+            [f"static/qrs/{cert_id}_{n}.png" for n in range(1, 7)]
+        ))
+        QR.objects.create(link=link, **kwargs)
 
-        return Response({
-            'image_1': f'{qr_1_path}',
-            'image_2': f'{qr_2_path}',
-            'image_3': f'{qr_3_path}',
-            'image_4': f'{qr_4_path}',
-            'image_5': f'{qr_5_path}',
-            'image_6': f'{qr_6_path}',
-        })
+        return Response(dict(zip(
+            [f"image_{n}" for n in range(1, 7)],
+            [kwargs[f"qr_{n}"] for n in range(1, 7) ]
+        )))
