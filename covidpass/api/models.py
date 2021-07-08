da1ed680b6cc0652a2ab245acf1be949e44344cf
Nick Avdeev <nikitaavdeev2000@gmail.com>
2021-07-08 18:20:04 +0300

    Добавлена генерация QR-кода,

--- covidpass/api/models.py
+++ covidpass/api/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 class QR(models.Model):
     link = models.URLField('Ссылка для QR-кода')
-    qr = models.ImageField('Сгенерированный код', upload_to='static/qrs')
-    datetime = models.DateTimeField('Время создания кода', auto_created=True)
+    qr_1 = models.ImageField('Сгенерированный код 1', upload_to='static/qrs')
+    qr_2 = models.ImageField('Сгенерированный код 2', upload_to='static/qrs')
+    qr_3 = models.ImageField('Сгенерированный код 3', upload_to='static/qrs')
+    qr_4 = models.ImageField('Сгенерированный код 4', upload_to='static/qrs')
+    qr_5 = models.ImageField('Сгенерированный код 5', upload_to='static/qrs')
+    qr_6 = models.ImageField('Сгенерированный код 6', upload_to='static/qrs')
+    datetime = models.DateTimeField('Время создания кода', auto_created=True, default=timezone.now())
 
     class Meta:
         verbose_name = 'QR-код'
